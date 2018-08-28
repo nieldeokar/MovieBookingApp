d5122c6623ada58e77ff0be5cb412c999155d37a
Nilesh deokar <nieldeokar@gmail.com>
2018-08-28 09:12:19 +0200

    added movie detail activity

---
+++ app/src/main/java/com/tentwenty/movieticket/feature/moviedetail/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.tentwenty.movieticket.feature.moviedetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
